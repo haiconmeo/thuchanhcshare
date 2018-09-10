d304759b983bf6afac666e76aed0649b1272dcec
Hoàng Hữu Mạnh <hoanghuumanh45@gmail.com>
2018-09-10 15:46:29 +0700

    uploat bai 17

---
+++ buoi1_hhm/buoi1_bai17/buoi1_bai17/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buoi1_bai17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("nhap n:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("ket qua la :{0}",n*(n+1)/2);
+            Console.ReadKey();
+        }
+    }
+}
