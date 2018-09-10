8539a74c54f606833544d94291e17164749fd37e
Hoàng Hữu Mạnh <hoanghuumanh45@gmail.com>
2018-09-10 15:53:42 +0700

    uploat bai 18

---
+++ buoi1_hhm/buoi1_bai18/buoi1_bai18/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buoi1_bai18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("nhap n:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int ketqua = 0;
+            for(int i = 1; i < n; i++)
+            {
+                ketqua += i * (i + 1);
+            }
+            Console.WriteLine("ket qua la :{0}", ketqua);
+            Console.ReadKey();
+        }
+    }
+}
