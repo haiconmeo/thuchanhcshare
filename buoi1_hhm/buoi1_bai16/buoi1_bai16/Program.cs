69758176055848b22c702c2541c371da6fcdaa6e
Hoàng Hữu Mạnh <hoanghuumanh45@gmail.com>
2018-09-10 15:39:55 +0700

    up bai 16

---
+++ buoi1_hhm/buoi1_bai16/buoi1_bai16/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buoi1_bai16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("nhap so :");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int tich = 1;
+            for (int i = 1; i <= a; i++)
+            {
+                tich *= i;
+            }
+            Console.WriteLine("ket qua la :{0}", tich);
+            Console.ReadKey();
+        }
+    }
+}
